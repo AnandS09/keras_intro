a39fa5dbe73ac8d1400389be7897129ea98cd159
Ananda Samajdar <anand@Anandas-MacBook-Pro.local>
2017-04-21 22:06:37 -0400

    Working tensorboard

--- tf_tutorial/keras_mnist.py
+++ tf_tutorial/keras_mnist.py
@@ -4,10 +4,12 @@ import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Activation
-from  keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Convolution2D, MaxPooling2D
 
 from keras.utils import np_utils
+from keras.callbacks import TensorBoard
 from keras.datasets import mnist
+
 
 # Data set and preprocessing
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
@@ -20,8 +22,12 @@ X_train /= 255
 X_test  = X_test.astype('float32')
 X_test /= 255
 
+# Smaller sized dataset to check how tensorboard works
+x_train = X_train[:1000, :]
+y_train = Y_train[:1000]
 
 Y_train = np_utils.to_categorical(Y_train, 10)
+y_train = np_utils.to_categorical(y_train, 10)
 Y_test  = np_utils.to_categorical(Y_test, 10)
 
 #Define the model here
@@ -41,8 +47,9 @@ model.add(Dense(10, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #Train the model
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, verbose=1)
-
+history = model.fit(x_train, y_train, batch_size=100, nb_epoch=100, verbose=1,
+                    callbacks=[TensorBoard(log_dir='./tensorboard_log', histogram_freq=1)])
+print(history)
 #See the output on test data
 score = model.evaluate(X_test, Y_test, verbose=0)
 
